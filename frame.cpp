7e05b0d33ef6c8c51a6e3b9e103b8ae4c594eede
Jesse Huang <jeshuang@eden.rutgers.edu>
2013-05-24 17:02:54 -0400

    2-4 modifications

--- frame.cpp
+++ frame.cpp
@@ -9,29 +9,29 @@ using std::string;
 int main()
 {
      cout << "Please ener your first name: ";
-
+     
      string name;
      cin >> name;
-
+     
      int padR;
      int padC;
      cout << "Please enter row spacing: ";
      cin >> padR;
      cout << "please enter column spacing: ";
      cin >> padC;
-
+     
      const string greeting = "Hello, " + name + "!";
-
      const int rows = padR * 2 + 3;
-
+     
      const string::size_type cols = greeting.size() + padC * 2 + 2;
-
+     const string spaces(cols - 2, ' ');
+     
      cout << endl;
-
+     
      for (int r = 0; r != rows; ++r)
      {
           string::size_type c = 0;
-
+          
           while (c != cols) {
                if (r == padR + 1 && c == padC + 1) {
                     cout << greeting;
@@ -39,13 +39,26 @@ int main()
                } else {
                     if (r == 0 || r == rows - 1 ||
                         c == 0 || c == cols - 1)
+                    {
                          cout << "*";
+                         ++c;
+                    }
                     else
-                         cout << " ";
-                    ++c;
+                    {
+                         if (r == padR + 1)
+                         {
+                              cout << " ";
+                              ++c;
+                         }
+                         else
+                         {
+                              cout << spaces;
+                              c += spaces.size();
+                         }
+                    }
                }
           }
-
+          
           cout << endl;
      }
      return 0;
