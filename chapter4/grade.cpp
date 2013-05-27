4e5a497b5ae6ab285d551116ce1ab2a01cadcfab
Jesse Huang <jeshuang@eden.rutgers.edu>
2013-05-26 20:08:50 -0400

    added grade files

---
+++ chapter4/grade.cpp
@@ -0,0 +1,21 @@
+#include "grade.h"
+#include "median.h"
+#include "Student_info.h"
+#include <vector>
+
+
+// Standard grading policy
+double grade(double midterm, double final, double hw)
+{
+     return 0.2 * midterm + 0.4 * final + 0.4 * hw;
+}
+
+double grade(double midterm, double final, const std::vector<double>& hw)
+{
+     return grade(midterm, final, median(hw));
+}
+
+double grade(const Student_info& s)
+{
+     return grade(s.midterm, s.final, s.homework);
+}
